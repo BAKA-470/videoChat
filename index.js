5f988e1d87b62ae56d68f74a2f046c3e10266f04
BAKA-470 <vivekjoshi2267735@gmail.com>
2023-08-05 13:37:58 +0530

    again imports

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const express = require('express');
 const app = express();
+const port = 3000;
+
+const server = require('http').Server(app);
+
+app.get('/', (req, res) => {
+    res.status(200).send('sup nigga!!!');
+
+})
+
+
+
+
+
+
+app.listen(port, function(err) {
+    if (err) {
+        console.log("Error in running the server:", err);
+    }
+    console.log(`Server is running on port: ${port}`);
+});
